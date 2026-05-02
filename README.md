33ebf6d71dc51c43bd8a01117daab923fd6712ba
Arthur Zheng <arthurzheng150@gmail.com>
2026-05-02 13:17:40 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# APDS9960
+
+![Version](https://img.shields.io/github/v/tag/openblock-plugin/apds9960?label=version)
+![License](https://img.shields.io/badge/license-MIT-green.svg)
+![Type](https://img.shields.io/badge/type-extension-orange.svg)
+![Tags](https://img.shields.io/badge/tags-sensor-lightgrey.svg)
+
+Proximity, Light, RGB, and Gesture Sensor which uses an I2C interface.
+
+## Documentation
+
+For detailed usage instructions, please visit:
+
+**[OpenBlock Wiki](https://wiki.openblock.cc)**
---
+++ src/translations.js
@@ -0,0 +1,95 @@
+/* eslint-disable quote-props */
+/* eslint-disable max-len */
+/**
+ * Translation file for this resouce.
+ *
+ * IMPORTANT:
+ * - The "en" (English) section is automatically generated from source files.
+ *   Do NOT modify the "en" section manually.
+ * - Other language sections (e.g., "zh-cn", "zh-tw", "ja", etc.) should be
+ *   manually translated by you.
+ * - When you run the extraction script again, only the "en" section will be
+ *   updated. Your manual translations in other languages will be preserved.
+ *
+ * Structure:
+ * - interface: translations for name/description (used by GUI formatMessage)
+ * - extensions: translations for extension blocks (used by VM formatMessage)
+ * - blocks: translations for Blockly blocks (used by Blockly.Msg)
+ */
+
+export default {
+    'interface': {
+        'en': {
+            'apds9960.description': 'Proximity, Light, RGB, and Gesture Sensor which uses an I2C interface.'
+        },
+        'ru': {
+            'apds9960.description': 'Proximity, Light, RGB, and Gesture Sensor which uses an I2C interface.'
+        },
+        'zh-cn': {
+            'apds9960.description': 'Proximity, Light, RGB, and Gesture Sensor which uses an I2C interface.'
+        },
+        'zh-tw': {
+            'apds9960.description': 'Proximity, Light, RGB, and Gesture Sensor which uses an I2C interface.'
+        }
+    },
+    'extensions': {
+        'en': {},
+        'ru': {},
+        'zh-cn': {},
+        'zh-tw': {}
+    },
+    'blocks': {
+        'en': {
+            'APDS9960_CATEGORY': 'APDS9960',
+            'APDS9960_INIT': 'init apds9960',
+            'APDS9960_ISGESTUREAVAILABLE': 'apds9960 gesture data ready?',
+            'APDS9960_READGESTURE': 'apds9960 read gesture',
+            'APDS9960_ISGESTURE': 'apds9960 read gesture %1?',
+            'APDS9960_GESTURE_UP': 'up',
+            'APDS9960_GESTURE_DOWN': 'down',
+            'APDS9960_GESTURE_LEFT': 'left',
+            'APDS9960_GESTURE_RIGHT': 'right',
+            'APDS9960_ISPROXIMITYAVAILABLE': 'apds9960 color data available?',
+            'APDS9960_READPROXIMITY': 'apds9960 read proximity'
+        },
+        'ru': {
+            'APDS9960_CATEGORY': 'APDS9960',
+            'APDS9960_INIT': 'init apds9960',
+            'APDS9960_ISGESTUREAVAILABLE': 'apds9960 gesture data ready?',
+            'APDS9960_READGESTURE': 'apds9960 read gesture',
+            'APDS9960_ISGESTURE': 'apds9960 read gesture %1?',
+            'APDS9960_GESTURE_UP': 'up',
+            'APDS9960_GESTURE_DOWN': 'down',
+            'APDS9960_GESTURE_LEFT': 'left',
+            'APDS9960_GESTURE_RIGHT': 'right',
+            'APDS9960_ISPROXIMITYAVAILABLE': 'apds9960 color data available?',
+            'APDS9960_READPROXIMITY': 'apds9960 read proximity'
+        },
+        'zh-cn': {
+            'APDS9960_CATEGORY': 'APDS9960',
+            'APDS9960_INIT': 'init apds9960',
+            'APDS9960_ISGESTUREAVAILABLE': 'apds9960 gesture data ready?',
+            'APDS9960_READGESTURE': 'apds9960 read gesture',
+            'APDS9960_ISGESTURE': 'apds9960 read gesture %1?',
+            'APDS9960_GESTURE_UP': 'up',
+            'APDS9960_GESTURE_DOWN': 'down',
+            'APDS9960_GESTURE_LEFT': 'left',
+            'APDS9960_GESTURE_RIGHT': 'right',
+            'APDS9960_ISPROXIMITYAVAILABLE': 'apds9960 color data available?',
+            'APDS9960_READPROXIMITY': 'apds9960 read proximity'
+        },
+        'zh-tw': {
+            'APDS9960_CATEGORY': 'APDS9960',
+            'APDS9960_INIT': 'init apds9960',
+            'APDS9960_ISGESTUREAVAILABLE': 'apds9960 gesture data ready?',
+            'APDS9960_READGESTURE': 'apds9960 read gesture',
+            'APDS9960_ISGESTURE': 'apds9960 read gesture %1?',
+            'APDS9960_GESTURE_UP': 'up',
+            'APDS9960_GESTURE_DOWN': 'down',
+            'APDS9960_GESTURE_LEFT': 'left',
+            'APDS9960_GESTURE_RIGHT': 'right',
+            'APDS9960_ISPROXIMITYAVAILABLE': 'apds9960 color data available?',
+            'APDS9960_READPROXIMITY': 'apds9960 read proximity'
+        }
+    }
+};
